5bffb10dc7d59e54651c129ec786ac9deb9275cc
dave-v <49401409+dave-v@users.noreply.github.com>
2023-08-04 10:34:16 +0100

    build: bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="django-tenants-celery-beat",
-    version="0.2.0",
+    version="0.2.1",
     author="David Vaughan",
     author_email="david.vaughan@quickrelease.co.uk",
     maintainer="Quick Release (Automotive) Ltd.",
